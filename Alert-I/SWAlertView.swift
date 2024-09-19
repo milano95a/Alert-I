39b07feef6912eb58d6e29704924bfc2eff2ab17
Jamoliddinov Abduraxmon <abduraxmon.jamoliddinov@smartbank.uz>
2024-09-19 12:01:16 +0500

    init

---
+++ Alert-I/SWAlertView.swift
@@ -0,0 +1,8 @@
+//
+//  SWAlertView.swift
+//  Alert-I
+//
+//  Created by Jamoliddinov Abduraxmon on 19/09/24.
+//
+
+import Foundation
---
+++ Alert-I/View+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  View+Extension.swift
+//  Alert-I
+//
+//  Created by Jamoliddinov Abduraxmon on 19/09/24.
+//
+
+import Foundation
