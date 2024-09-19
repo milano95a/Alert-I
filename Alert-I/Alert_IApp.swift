68bd8ec03e7b8deefbf6e1e5a26b9208a0035d9a
Jamoliddinov Abduraxmon <abduraxmon.jamoliddinov@smartbank.uz>
2024-09-19 11:24:08 +0500

    Initial Commit

---
+++ Alert-I/Alert_IApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alert_IApp.swift
+//  Alert-I
+//
+//  Created by Jamoliddinov Abduraxmon on 19/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Alert_IApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
